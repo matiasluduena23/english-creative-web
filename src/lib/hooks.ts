2f4ae7a00fd50d6503fe5661b8c97f6678fd1564
Matias Luduena <matiasluduena23@gmail.com>
2024-07-22 15:14:11 -0300

    fix build

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,50 +1,32 @@
 import { useEffect, useState } from 'react';
 
 export function useShowNavigation() {
-	const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false);
 
-	useEffect(() => {
-		let previousScrollPosition = 0;
-		let currentScrollPosition = 0;
-		window.addEventListener('scroll', () => {
-			currentScrollPosition = window.scrollY;
-			currentScrollPosition > previousScrollPosition
-				? setShow(false)
-				: setShow(true);
+  useEffect(() => {
+    let previousScrollPosition = 0;
+    let currentScrollPosition = 0;
+    window.addEventListener('scroll', () => {
+      currentScrollPosition = window.scrollY;
+      currentScrollPosition > previousScrollPosition
+        ? setShow(false)
+        : setShow(true);
 
-			previousScrollPosition = currentScrollPosition;
-		});
-	}, []);
-	return show;
+      previousScrollPosition = currentScrollPosition;
+    });
+  }, []);
+  return show;
 }
 
 export function useShadowNavigation() {
-	const [shadow, setShadow] = useState(false);
+  const [shadow, setShadow] = useState(false);
 
-	useEffect(() => {
-		window.addEventListener('scroll', () => {
-			let currentScroll = window.scrollY;
-			currentScroll > 100 ? setShadow(true) : setShadow(false);
-		});
-	}, []);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      let currentScroll = window.scrollY;
+      currentScroll > 100 ? setShadow(true) : setShadow(false);
+    });
+  }, []);
 
-	return shadow;
-}
-
-export function useMediaQuery(query: string) {
-	const [value, setValue] = useState(false);
-
-	useEffect(() => {
-		function onChange(event: MediaQueryListEvent) {
-			setValue(event.matches);
-		}
-
-		const result = matchMedia(query);
-		result.addEventListener('change', onChange);
-		setValue(result.matches);
-
-		return () => result.removeEventListener('change', onChange);
-	}, [query]);
-
-	return value;
+  return shadow;
 }
